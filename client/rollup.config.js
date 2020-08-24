be6a5670577f814cfc86ea76b2d3d8074c8b229e
고혜영 <popo1211@tmon.co.kr>
2020-08-25 02:06:04 +0900

    edit config, svelte tutorial

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -2,7 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser'; //생성된 ES 번들을 최소화
 
 
 const production = !process.env.ROLLUP_WATCH;
@@ -23,7 +23,12 @@ export default {
 			}
 		}),
 		resolve({
+			//browser default false; 
+			//true일 경우 "package.json" 파일의 "browser" 속성을 사용하여 번들을 위해 로드할 대체 파일을 지정
+			//브라우저 환경에서 번들링 할 때 유용
 			browser: true,
+			//dedupe default []
+			//의존성에 의해 패키지 import할 때 같은 패키지가 중복으로 들어가는 것을 방지
 			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
 		}),
 		commonjs(),
--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,54 @@
 <script>
-  let rand = -1;
+  import Nested from './Nested.svelte'
+  import Inner from './Inner.svelte'
 
+  let rand = -1;
   function getRand() {
     fetch("./rand")
       .then(d => d.text())
       .then(d => (rand = d));
   }
+
+  let count = 0;
+  $: doubled = count * 2;
+
+  function handleClick(){
+    count +=1;
+    
+  }
+  $:console.log( `the count is ${count}`);
+
+
+  function handleAlert(self) {
+    console.log( self );
+  }
+  
+  function handleMessage(event){
+    console.log( event);
+    alert(event.detail.text +"\n"+ event.detail.desc);
+  }
+
+  let name = '';
 </script>
 
 <h1>Your number is {rand}!</h1>
 <button on:click={getRand}>Get a random number</button>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+<p>
+  {count} doubled is {doubled}
+</p>
+
+<Nested />
+<br/>
+
+<button on:click|self={handleAlert}>
+print event.target
+</button>
+
+<Inner on:message={handleMessage} />
+
+<input bind:value={name} />
+<h2>내이름은 {name}</h2>
